3cd7a3012bbb5962d3fd602d9ee9564947d393ea
Vishal Bairagi <vshalbairagi@gmail.com>
2026-09-12 21:50:04 +0530

    Create 0389 - Find the Difference.py

---
+++ Python/0389 - Find the Difference.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        for i in t:
+            if s.count(i)!=t.count(i):
+                return i 
